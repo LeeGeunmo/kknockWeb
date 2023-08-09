298a45755dca833fa6f7442138e63c066d349e1a
Geunmo <rmsah0105@naver.com>
2023-08-09 21:44:54 +0900

    sessionId오류

--- content_modify.php
+++ content_modify.php
@@ -4,18 +4,16 @@ include ('dbConnect.php');
 
 $id = $_GET['id'];
 
-
-
-
 $query = "select * from board where id = '$id';";
 $res = mysqli_query($conn,$query);
 $row = mysqli_fetch_array($res);
 
-// echo $_SESSION['id']; echo "<br>";
-// echo $row['id']
+echo $_SESSION['id']; echo "<br>";
+echo $row['userId']; echo "<br>";
+
 if ($_SESSION['id'] != $row['userId']){
     echo "<script>alert('비정상적인 접근입니다.');</script>";
-    echo "<script>location.replace('board.php');</script>";
+    //echo "<script>location.replace('board.php');</script>";
 }
 ?>
 <!DOCTYPE html>
